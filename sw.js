8e12a4905bde7f669f5c9f9d76a3e2fc187387f9
Laviniu137 <laviniu137@gmail.com>
2026-09-17 16:52:15 +0300

    1.20.9

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = 'fisa-atelier-v225';
+const CACHE_NAME = 'fisa-atelier-v226';
 const APP_FILES = [
   './',
   './index.html',
   './sw.js',
-  './notes/notes-editor.css?v=1.20.8',
-  './notes/notes-editor.js?v=1.20.8',
+  './notes/notes-editor.css?v=1.20.9',
+  './notes/notes-editor.js?v=1.20.9',
   './notes/pdf-lib.min.js',
   './manifest.webmanifest',
-  './icon-180.png?v=1.20.8',
-  './icon-192.png?v=1.20.8',
-  './icon-512.png?v=1.20.8'
+  './icon-180.png?v=1.20.9',
+  './icon-192.png?v=1.20.9',
+  './icon-512.png?v=1.20.9'
 ];
 
 self.addEventListener('install', (event) => {
